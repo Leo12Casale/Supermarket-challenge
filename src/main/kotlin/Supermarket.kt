7d0ee2258910b639b3a950362efec834e27efa2d
LeonelCasale <leonelcasale@gmail.com>
2024-08-06 10:44:55 -0300

    Creation of project structure
    Move the root source from /src to /src/main/kotlin and creation of the classes

---
+++ src/main/kotlin/Supermarket.kt
@@ -0,0 +1,111 @@
+import java.time.LocalTime;
+import java.time.DayOfWeek;
+
+class Supermarket(
+        val id: Int,
+        val name: String
+
+) {
+    //List of products shared by all the supermarkets
+    companion object {
+        val products: MutableList<Product> = mutableListOf()
+
+        fun addProduct(product: Product) {
+            if (products.none { it.id == product.id })
+                products.add(product)
+        }
+    }
+
+    internal val sales: MutableMap<Product, Int> = mutableMapOf()  //Product and quantity sold
+    private val revenue: MutableMap<Product, Double> = mutableMapOf()  //Product and total revenue
+    private val productsIdStock: MutableMap<Int, Int>  //Map of product ID and stock
+        get() {
+            return productsIdStock
+        }
+    internal val openHours: Pair<LocalTime, LocalTime>? = null  //Opening and closing times
+    internal val openDays: List<DayOfWeek>? = null  //Days when the supermarket is open
+
+    init {
+        //Initialize stock of each product with 10 units
+        products.forEach { product ->
+            if (!productsIdStock.containsKey(product.id))
+                productsIdStock[product.id] = 10
+        }
+    }
+
+    /**
+     * Method to register a sale.
+     * @property productId the ID of the product to sale.
+     * @property quantity the quantity to sale.
+     * @return total sale price.
+     */
+    fun registerSale(productId: Int, quantity: Int): Double {
+        //The product exists?
+        val product = getProductById(productId)
+        //Is there enough stock of that product in this supermarket?
+        val currentStock = productsIdStock[productId] ?: 0
+        if (currentStock < quantity) throw IllegalArgumentException("Insufficient stock for product ID $productId.")
+
+        //Update stock, sales amount and revenue of the supermarket
+        productsIdStock[productId] = currentStock - quantity
+        sales[product] = sales.getOrDefault(product, 0) + quantity
+        revenue[product] = revenue.getOrDefault(product, 0.0) + product.price * quantity
+
+        return product.price * quantity
+    }
+
+    /**
+     * Method to return the quantity sold of a particular product
+     * @property productId the ID of the product sold.
+     * @return quantity sold.
+     */
+    fun getQuantitySold(productId: Int): Int {
+        val product = getProductById(productId)
+        return sales[product] ?: 0
+    }
+
+    /**
+     * Method to return revenue from sales of a given product
+     * @property productId the ID of the product sold.
+     * @return amount of revenue.
+     */
+    fun getRevenueByProduct(productId: Int): Double {
+        val product = getProductById(productId)
+        return revenue[product] ?: 0.0
+    }
+
+    /**
+     * Method to return total revenue from sales
+     * @return amount of total revenue.
+     */
+    fun getTotalRevenue(): Double {
+        return revenue.values.sum()
+    }
+
+    /**
+     * Method to determinate if a supermarket is open.
+     * @property day day of the week
+     * @property time time of the day
+     * @return false if it's closed or true if it's open.
+     */
+    fun isSupermarketOpen(day: DayOfWeek, time: LocalTime): Boolean {
+        return openDays?.contains(day) == true && openHours?.let { (open, close) ->
+            time >= open && time <= close
+        } == true
+    }
+
+    /**
+     * Method to get a given product by its id.
+     * @property productId id of the product.
+     * @return the product
+     * @throws IllegalArgumentException if the product is not found
+     */
+    fun getProductById(productId: Int) : Product {
+        return products.find { it.id == productId }
+                ?: throw IllegalArgumentException("Product with ID $productId not found.")
+    }
+
+    fun getProducts() : MutableList<Product> {
+        return products;
+    }
+}
---
+++ src/main/kotlin/SupermarketChain.kt
@@ -0,0 +1,61 @@
+import java.lang.RuntimeException
+import java.time.DayOfWeek
+import java.time.LocalTime
+
+class SupermarketChain(val supermarkets: List<Supermarket>) {
+
+    /**
+     * Method to get the top 5 best-selling products
+     * @return a String with the name and quantity of each product
+     */
+    fun getTop5Products(): String {
+        //Create a map to accumulate the quantity sold of each product in the entire chain
+        val productSales = mutableMapOf<Int, Int>()
+
+        //Iterate each supermarket and accumulate the solds
+        supermarkets.forEach { supermarket ->
+            supermarket.sales.forEach { (product, quantity) ->
+                productSales[product.id] = productSales.getOrDefault(product.id, 0) + quantity
+            }
+        }
+
+        //Get all the products
+        val productById = supermarkets.getOrElse(0, throw RuntimeException("There is no supermarkets in the chain.")).getProducts().associateBy { it.id }
+
+        //Get the top 5 of sales of each product in the entire chain
+        return productSales.entries
+                .sortedByDescending { it.value }  //Order desc
+                .take(5)  //Take first five
+                .joinToString("-") { entry ->
+                    val product = productById[entry.key]
+                    "${product?.name ?: "Unknown"}: ${entry.value}"
+                }
+    }
+
+    /**
+     * Method to get the total revenue in the entire chain
+     * @return the total revenue
+     */
+    fun getTotalRevenue(): Double {
+        return supermarkets.sumOf { it.getTotalRevenue() }
+    }
+
+    /**
+     * Method to get the supermarket with more sales revenue
+     * @return the name of the supermarket and the revenue
+     */
+    fun getTopSupermarket(): String {
+        val topSupermarket = supermarkets.maxByOrNull { it.getTotalRevenue() }
+                ?: throw IllegalArgumentException("No supermarkets available.")
+        return "${topSupermarket.name} (${topSupermarket.id}). Total revenue: ${topSupermarket.getTotalRevenue()}"
+    }
+
+    /**
+     * Method to get the total revenue in the entire chain
+     * @return the total revenue
+     */
+    fun getOpenSupermarkets(day: DayOfWeek, time: LocalTime): String {
+        return supermarkets.filter { it.isSupermarketOpen(day, time) }
+                .joinToString(", ") { "${it.name} (${it.id})" }
+    }
+}
